4828ba154279a99a6296b3955fd1865045b3861a
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2022-10-04 05:33:42 +0100

    Basic fetch from API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import React from 'react';
 
 export default class App extends React.Component {
 
+  apiOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+      'X-RapidAPI-Host': process.env.REACT_APP_API_URL
+    }
+  };
+
+  lastFilters = null
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: this.fetchGames([])
+    }
+  }
+
+  fetchGames(filters) {
+    fetch(`https://${process.env.REACT_APP_API_URL}/api/games`, this.apiOptions)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }
+
   render() {
     return <div>Hello World</div>
   }
