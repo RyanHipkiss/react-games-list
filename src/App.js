2b2bcaa4bded70a6156b87e9a2831cee92357080
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2022-10-04 05:11:58 +0100

    Update App.js to remove redundant code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useEffect } from 'react';
 
 export default class App extends React.Component {
 
   render() {
     return <div>Hello World</div>
-    // return <div>Hello World</div>
   }
 }
