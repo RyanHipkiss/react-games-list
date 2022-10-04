ebbf6b186d31532633e24167ab19f40619568001
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2022-10-04 13:57:04 +0100

    basic render and test

--- src/App.js
+++ src/App.js
@@ -15,18 +15,25 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      games: this.fetchGames([])
+      games: []
     }
   }
 
+  componentDidMount() {
+    this.fetchGames([]).then(result => this.setState({
+      games: result
+    }))
+  }
+
   fetchGames(filters) {
-    fetch(`https://${process.env.REACT_APP_API_URL}/api/games`, this.apiOptions)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
+    return fetch(`https://${process.env.REACT_APP_API_URL}/api/games`, this.apiOptions)
+      .then(response => {
+        return response.json()
+      })
+      .catch(err => console.error(err))
   }
 
   render() {
-    return <div>Hello World</div>
+    return <div data-testid="json">{JSON.stringify(this.state.games)}</div>
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { act } from 'react-dom/test-utils';
 
-test('renders text', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Hello World/i);
-  expect(textElement).toBeInTheDocument();
+const server = setupServer(
+  rest.get(`https://${process.env.REACT_APP_API_URL}/api/games`, (req, res, ctx) => {
+    return res(ctx.json({test: 'hello'}))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('renders text', async () => {
+
+  await act(async() => {
+    render(<App/>)
+  })
+
+  await waitFor(() => {
+    expect(JSON.parse(screen.getByTestId('json').textContent)).toStrictEqual({test: 'hello'})
+  })
 });
